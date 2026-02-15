5059c593ee6bbfda6ee0c42ea25122dccb0fc7a7
Samuel Huwiler <samuel@MacBook-Pro-von-Samuel.local>
2026-02-15 17:28:19 +0100

    pyproject chagnes and main.py for testing

--- src/examtracker/main.py
+++ src/examtracker/main.py
@@ -7,9 +7,10 @@ from examtracker.settings import Settings
 
 def main() -> int:
     config: Settings = Settings()
-    db_engine = create_database_engine(config.database_path)
-    app = ExamTracker(db_engine, config)
-    app.run()
+    print(config)
+    # db_engine = create_database_engine(config.database_path)
+    # app = ExamTracker(db_engine, config)
+    # app.run()
     return 0
 
 
